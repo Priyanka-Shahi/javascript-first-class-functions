0fe0bd0eb7b31820fbe22e1e75a446882eeec233
Priyanka-Shahi <shahipriyan@gmail.com>
2022-07-15 19:24:59 +1000

    First Class Function

---
+++ index.js
@@ -0,0 +1,19 @@
+const { spyOn } = require("expect/lib");
+
+function receivesAFunction(callback) {
+
+    callback()
+}
+
+
+function returnsANamedFunction() {
+return (returnsANamedFunction)
+
+}
+
+function returnsAnAnonymousFunction() {
+    return function() {
+        console.log("return anon");
+    }
+}
+
